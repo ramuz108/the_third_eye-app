29edb0fd23d3e2920c36b80c4660d75bf389e1ba
Ramachandran A <ramhero22@gmail.com>
2020-08-08 18:22:31 +0530

    Update detect2.py

--- CONTROL_STATION_WEB/detect2.py
+++ CONTROL_STATION_WEB/detect2.py
@@ -1,32 +1,26 @@
+#Author: Ramachandran A Dr.Gireeshan MG
+#Opens camera in the back-end and detect for objects using a Machine Learning Model 
+#uses Tensorflow,Keras,OpenCV and Matapolib
 #!/usr/bin/env python
-from imageai.Detection import VideoObjectDetection
+from imageai.Detection import VideoObjectDetection  #using imageAI
 import os
 import time as t
-import cv2
+import cv2 #using opencv
 import datetime
 import sys
-
-
-
-def forSeconds(second_number, output_arrays, count_arrays, average_output_count):
+def forSeconds(second_number, output_arrays, count_arrays, average_output_count): #callback function for every second
     x=output_arrays
-    print(x)
-    if x[0]['name']=='person' :
-        print('Got it!!!!')
-        sys.exit("person") 
-    print("------------END OF A FRAME --------------")
-
-
-
+    if x[0]['name']=='person' or x[0]['name']=='bicycle' or x[0]['name']=='car' or x[0]['name']=='bus' or x[0]['name']=='motorcycle' or x[0]['name']=='truck':
+        sys.exit(x[0]['name']) #provide the output with the threat
 execution_path = os.getcwd()
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0) #opening camera. Uses webcam for demonstration ;-)
 
 detector = VideoObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+detector.setModelPath(os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5")) #retinanet model used for detection
 detector.loadModel()
 
-
+# imageAI detection function with detection probability of 30%
 detections = detector.detectObjectsFromVideo(camera_input=camera,save_detected_video=False,frames_per_second=20,per_frame_function=forSeconds, minimum_percentage_probability=30, return_detected_frame=True)
 
 

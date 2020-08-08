4c6e5f57cd25fc467afa2e87b3817546acbf3883
Ramachandran A <ramhero22@gmail.com>
2020-08-08 12:27:15 +0530

    Update Globals.java

--- ANDROID/java/com/example/thethirdeye/Globals.java
+++ ANDROID/java/com/example/thethirdeye/Globals.java
@@ -1,15 +1,17 @@
+//Author: Ramachandran A Dr.Gireeshan MG
+//Class to save the global ip address throughout the application instance
 package com.example.thethirdeye;
 
 import android.app.Application;
 
 public class Globals extends Application {
-    private String data="192.168.10.8";
+    private String data=""; //initialized the variable
 
-    public String getData(){
+    public String getData(){       //getter
         return this.data;
     }
 
-    public void setData(String d){
+    public void setData(String d){ //setter
         this.data=d;
     }
 }

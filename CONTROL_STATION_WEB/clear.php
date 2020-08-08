2f8cf3b78535e55a17a2b9b7529d6facca7e1101
Ramachandran A <ramhero22@gmail.com>
2020-08-08 18:48:19 +0530

    Update clear.php

--- CONTROL_STATION_WEB/clear.php
+++ CONTROL_STATION_WEB/clear.php
@@ -1,10 +1,12 @@
+<!--Author: Ramachandran A Dr.Gireeshan MG-->
+<!--Script to clear all the alarms from the database-->
 <!DOCTYPE html>
 <html>
   <head>
     <title>The Third Eye - Clear Alarms</title>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./police.css"
+  <link rel="stylesheet" href="./police.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
 <style>

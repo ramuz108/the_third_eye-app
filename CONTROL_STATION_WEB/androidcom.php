60749378f4412956254e8f9b755b591cba645f87
Ramachandran A <ramhero22@gmail.com>
2020-08-08 19:42:30 +0530

    Add files via upload

---
+++ CONTROL_STATION_WEB/androidcom.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "thirdeye";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT * from alarms";
+$result = $conn->query($sql);
+foreach($result as $item)
+{
+    $res['data'][] = array(
+     
+         'alarm'=>$item['alarm'],
+         'time'=>$item['details'], 
+         'lat'=>$item['lat'],
+         'lon'=>$item['lon']
+
+    );
+   
+}
+$myJSON = json_encode($res);
+echo $myJSON;
+$conn->close();
+?>

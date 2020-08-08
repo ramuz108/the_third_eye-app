22895eaa380105267b6202801f4c89023c4b4b64
Ramachandran A <ramhero22@gmail.com>
2020-08-08 12:23:24 +0530

    Update passview.java
    code documentation

--- ANDROID/java/com/example/thethirdeye/passview.java
+++ ANDROID/java/com/example/thethirdeye/passview.java
@@ -1,3 +1,5 @@
+//Author: Ramachandran A  Dr.Gireeshan M.G
+//Activity to check the user credentials - Launcher activity
 package com.example.thethirdeye;
 
 import android.content.Intent;
@@ -14,7 +16,7 @@ import android.view.View;
 import android.widget.Toast;
 
 public class passview extends AppCompatActivity {
-PasscodeView passcodeView;
+PasscodeView passcodeView; //custom password view
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +27,17 @@ PasscodeView passcodeView;
         PasscodeView passcodeView = (PasscodeView) findViewById(R.id.passcodeView);
         passcodeView
                 .setPasscodeLength(4)
-                .setLocalPasscode("9749")
+                .setLocalPasscode("9749") //passcode integrated
                 .setListener(new PasscodeView.PasscodeViewListener() {
                     @Override
                     public void onFail() {
-                       Toast.makeText(getApplication(),"You cant enter third eye with that password !!!!",Toast.LENGTH_LONG).show();
+                       Toast.makeText(getApplication(),"You can't enter third eye with that password !!!!",Toast.LENGTH_LONG).show();
                     }
 
                     @Override
                     public void onSuccess(String number) {
                       Intent i = new Intent(getApplication(),front_screen.class);
-                      startActivity(i);
+                      startActivity(i); //activity to enter the ip address
                     }
                 });
 
